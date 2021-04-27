cf9868b9e300e3718d5ed1ff50dbe44b20e8514f
Luke Ebbinghaus <luke.m.ebbinghaus@biola.edu>
2021-04-27 14:25:12 -0700

    Added Licensing Headers

--- database.cpp
+++ database.cpp
@@ -1,3 +1,22 @@
+/*
+    database.cpp
+    Part of Heavid
+    Copyright (C) 2021  Misheel Bayarsaikhan, Keziah Charis Bulseco, Luke Ebbinghaus, Vanessa Garcia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "database.h"
 #include <QTextStream>
 
@@ -90,7 +109,7 @@ std::unordered_map<std::string, std::string> * database::readServerInfo()
             config->at("server") = lines.substr(passwordLoc, lines.substr(passwordLoc, lines.length() - passwordLoc).find("\n"));
         }
     }  catch (std::out_of_range &e) {
-        std::cout << "WARNING: Database config file is missing or unreadable. "
+        std::cout << "WARNING: database.ini is missing or unreadable. "
             "Using default credentials." << std::endl;
     }
 
